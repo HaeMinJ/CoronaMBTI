2c18b2a2e5be4cfbd8b197883da02d2524ab65e2
kye8524 <kye8314@gmail.com>
2020-07-25 10:01:58 +0900

    자바스크립트추가

--- js/question.js
+++ js/question.js
@@ -1,32 +1,93 @@
-let questionList = new Array(4);
-for(let i = 0 ; i < questionList.length ; i++){
-    questionList[i] = new Array(5);
-    for(let j = 0; j < questionList[i].length ; j++){
-        questionList[i][j]=`${i}+${j}`;
-    }
-}
-let [ei, sn, tf, jp] = [0,0,0,0];
-let count = 0;
+var startPageIdx = 0;
+    var answerStatus = {
+        e: 0,
+        s: 0,
+        t: 0,
+        j: 0
+    } // 동의 +3 ~ -3 비동의
 
-var nextButton = new Vue({
-
-});
-
-var q = new Vue({
-    el: ".question",
-    data:{
-        questions : ['1','2','3','4','5']
-
-    },
-    method : {
-        nextQuestion : function () {
-            for (question in questionList){
-                data.questions.push({message:question});
-                console.log(question);
+    function onNextClicked() {
+      var p=document.getElementById("pg");
+      p.value=p.value+25;
+      document.getElementById( 'pg' ).value = p.value;
+        app.questions.forEach(function (question) {
+            switch (question.type) {
+                case 0:
+                    answerStatus.e += Number.parseInt(question.status, 0);
+                    break;
+                case 1:
+                    answerStatus.s += Number.parseInt(question.status, 0);
+                    break;
+                case 2:
+                    answerStatus.t += Number.parseInt(question.status, 0);
+                    break;
+                case 3:
+                    answerStatus.j += Number.parseInt(question.status, 0);
+                    break;
             }
+
+        });
+        startPageIdx++;
+        getJson(startPageIdx);
+        if(startPageIdx == 3){
+            document.getElementById('nextButton').innerHTML = "<span>완료</span>";
+            document.getElementById('nextButton').onclick = onCompleteClicked;
         }
-    },
-    created : function () {
-        q.computed.nextQuestion();
+        for(let el of document.getElementsByClassName('radio')){
+            el.classList.remove('selected');
+        }
+
+        for(let el of document.getElementsByClassName('neutral')){
+            el.classList.add('selected');
+        }
+        window.scrollTo(0, 0);
     }
-});
+    function onCompleteClicked() {
+        location.href = './mbti_result.html?e='+answerStatus.e+'&s='+answerStatus.s+"&t="+answerStatus.t+"&j="+answerStatus.j;
+        console.log("E : "+answerStatus.e +" S : "+answerStatus.s +" T :" +answerStatus.t+" J :"+answerStatus.j);
+    }
+
+    function optionClicked(event, item, option) {
+        item.status = option;
+        for(let el of event.target.parentElement.getElementsByClassName('radio')){
+            el.classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+    }
+    function getJson(startPage) {
+        fetch('http://static-yojongdo.coronambti.com/json/' + 'mbti_page' + startPage + '.json')
+            .then(response =>{
+                return response.json();
+            })
+        .then(data => {
+            app.questions = data;
+        });
+    }
+    Vue.component('question-item', {
+        props: ['question'],
+        template: '<div class = "question_container"><div class="statement">' +
+            '            <!--질문멘트-->{{ question.statement }}' +
+            '        </div>' +
+            '        <div class="decision">' +
+            '            <span class="captionAgree">동의</span>' +
+            '            <div class="options">' +
+            '                <div class="agreeMax radio agree" v-on:click="optionClicked($event, question,3)"><span class="check" ></span></div>' +
+            '                <div class="agreeMed radio agree" v-on:click="optionClicked($event, question,2)"><span class="check"></span></div>' +
+            '                <div class="agreeMin radio agree" v-on:click="optionClicked($event, question,1)"><span class="check"></span></div>' +
+            '                <div class="neutral radio selected" v-on:click="optionClicked($event, question,0)"><span class="check"></span></div>' +
+            '                <div class="disagreeMin radio disagree" v-on:click="optionClicked($event, question,-1)"><span class="check"></span></div>' +
+            '                <div class="disagreeMed radio disagree" v-on:click="optionClicked($event, question,-2)"><span class="check"></span></div>' +
+            '                <div class="disagreeMax radio disagree" v-on:click="optionClicked($event, question,-3)"><span class="check"></span></div>' +
+            '            </div>' +
+            '            <span class="captionDisagree">비동의</span>' +
+            '        </div>' +
+            '        <hr/></div>'
+    });
+    var app = new Vue({
+        el: '.question',
+        data: {
+            questions: []
+        }
+    })
+
+    getJson(startPageIdx);
